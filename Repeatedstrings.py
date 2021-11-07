39f742971f631ab1c987ebdbb9de1fe4c9f2cde9
Anatoliy Serputov <aserputov@myseneca.ca>
2021-11-06 22:11:55 -0400

    Eight Tests Passed

--- Repeatedstrings.py
+++ Repeatedstrings.py
@@ -4,22 +4,33 @@ import time
 
 def fun(s, n):
     count = 0
-    if s.count('a') != len(s):
-        length = ""
-        for j in range(n):
-            length = length + s
-            if len(length) > 10:
-                length = length[0:10]
-                break
-
-        print(length)
-
-        for i in length:
-            if i == 'a':
-                count += 1
-        count = count * n
+    if len(s) > n:
+        s = s[0:10]
+        count = s.count('a')
     else:
-        count = len(s) * n
+        if(len(s) != s.count('a')):
+            check = n / len(s)
+            # print(check)
+            # print(len(s))
+            getter = int(check) * len(s)
+            # print(s)
+            s = s * int(check)
+            #  print(s, n)
+            # print(getter)
+            check = n - getter
+            count = s.count('a')
+            # print(check)
+            if check != 0:
+                a = s[0:check]
+                count = a.count('a')
+                count = count + s.count('a')
+            # print(check)r
+            #     s = s * 2
+            #     print(s)
+            # print(count)
+            # s = s[0:n]
+        else:
+            count = n
 
     print(count)
 
@@ -29,6 +40,35 @@ num = int(input(""))
 fun(string, num)
 start_time = time.time()
 print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# def fun(s, n):
+#     count = 0
+#     if s.count('a') != len(s):
+#         length = ""
+#         for j in range(n):
+#             length = length + s
+#             if len(length) > 10:
+#                 length = length[0:10]
+#                 break
+
+#         print(length)å
+
+#         for i in length:
+#             if i == 'a':
+#                 count += 1
+#         count = count * n
+#     else:
+#         count = len(s) * n
+
+#     print(count)
+
+
+# string = input("")
+# num = int(input(""))
+# fun(string, num)
+# start_time = time.time()
+# print("--- %s seconds ---" % (time.time() - start_time))
 
 
 # Algorithm 1
